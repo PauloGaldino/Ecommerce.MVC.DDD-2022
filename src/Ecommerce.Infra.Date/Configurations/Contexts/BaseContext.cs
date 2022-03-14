e4145eedd82aafae403cad869659462c8bf1920f
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2022-03-14 17:10:04 -0300

    Implementada as classes na e ropsitórios na camada de infraestrutura.

---
+++ src/Ecommerce.Infra.Date/Configurations/Contexts/BaseContext.cs
@@ -0,0 +1,82 @@
+﻿using Ecommerce.Entity.Entities.Persons.Indentity.Users;
+using Ecommerce.Entity.Entities.Products;
+using Ecommerce.Entity.Entities.Shpos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infra.Date.Configurations.Contexts
+{
+    public class BaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public BaseContext(DbContextOptions<BaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserShop> UserShop { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Personalizando os nomes das tabelas No Identity
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser>(b =>
+            {
+                b.ToTable("Identity_Usuario");
+            });
+
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("Identity_UsuarioDeclaracao");
+
+            });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.ToTable("Identity_UsuarioLogins");
+            });
+
+            modelBuilder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.ToTable("Identity_UsuarioTokens");
+            });
+
+            modelBuilder.Entity<IdentityRole>(b =>
+            {
+                b.ToTable("Identity_Funcao");
+            });
+
+            modelBuilder.Entity<IdentityRoleClaim<string>>(b =>
+            {
+                b.ToTable("Identity_FuncaoDeclaracao");
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.ToTable("Identity_UsuarioFuncao");
+            });
+        }
+        private string GetStringConnectionConfig()
+        {
+            //Data Source=(localdb)\\MSSQLLocalDB
+            //Data Source=DESKTOP-PAULO\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            string strCon = "Data Source=DESKTOP-PAULO\\SQLSERVEREXPRESS;Initial Catalog=Ecommerce2021;Integrated Security=True";
+            return strCon;
+        }
+    }
+}
---
+++ src/Ecommerce.Infra.Date/Repositories/RProducts/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Interfaces.IProducts;
+using Ecommerce.Entity.Entities.Products;
+using Ecommerce.Infra.Date.Repositories.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infra.Date.Repositories.RProducts
+{
+    public class ProductRepository : GenericRepository<Product>, IProduct
+    {
+    }
+}
